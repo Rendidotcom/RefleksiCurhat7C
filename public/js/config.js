27714d3daf1566941b18048d55395419d4e9ce1c
Rendidotcom <holis112@gmail.com>
2026-01-13 12:15:06 +0700

    v17

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 // config.js — FINAL
 (function () {
   const API_URL =
-    "https://script.google.com/macros/s/AKfycbyiJ61oOwyzcjxMSWw63zlKc_HJHcs7YQ61R6sKuvc2gItWqN0OlxTDDr5zLpbCkd1RbA/exec";
+    "https://script.google.com/macros/s/AKfycby9PJYM7Pd8yDLWWsgO1xK9JzHmDGJPY6pZayeF_ff_yReKtQIHn7Uv3lf8-ghVCPuRpw/exec";
 
   window.API_URL = API_URL;
 })();
