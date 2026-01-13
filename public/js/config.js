d7e1853b89b3c05eed083dad9e5db649671a1e2e
Rendidotcom <holis112@gmail.com>
2026-01-13 09:19:42 +0700

    Refleksi V1

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 // config.js — FINAL
 (function () {
   const API_URL =
-    "https://script.google.com/macros/s/AKfycbx8bqoirLDJNBsYV5wmT_RwEarvGXhJn65N3zD7PdQZNjqv4-3-qbxxvYxWO7_eZv0cqg/exec";
+    "https://script.google.com/macros/s/AKfycbyiJ61oOwyzcjxMSWw63zlKc_HJHcs7YQ61R6sKuvc2gItWqN0OlxTDDr5zLpbCkd1RbA/exec";
 
   window.API_URL = API_URL;
 })();
