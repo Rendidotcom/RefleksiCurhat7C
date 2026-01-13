2ed4910a9e34face4e8cc731e3fb9e5b3071597d
Rendidotcom <holis112@gmail.com>
2026-01-13 12:30:35 +0700

    v21

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 // config.js — FINAL
 (function () {
   const API_URL =
-    "https://script.google.com/macros/s/AKfycby9PJYM7Pd8yDLWWsgO1xK9JzHmDGJPY6pZayeF_ff_yReKtQIHn7Uv3lf8-ghVCPuRpw/exec";
+    "https://script.google.com/macros/s/AKfycbwRHO0HPu433jTO9wUeKoaJFoYDDAmXR3zUU32bwhIVw6K-f-MlCDOBrO-4bE1qNnyxKA/exec";
 
   window.API_URL = API_URL;
 })();
